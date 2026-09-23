c5078e9293f91eddc474f24abb1c7f3dd1ebdd2e
Rodrigo Aljonas <rodrigo.aljonas17@gmail.com>
2026-09-23 14:13:00 -0300

    Começo do codigo JavaScript

---
+++ scr/js/script.js
@@ -0,0 +1,9 @@
+let NomeVinho = prompt("Digite o nome do vinho: ");
+let TipoVinho = prompt("Digite o tipo do vinho (Tinto, Branco, Rosé): ")
+let Safra = prompt("Digite a safra(ano): ")
+let QtdEstoque = prompt("Digite a quantidadade em estoque: ")
+
+alert("Cadastro realizado! Veja os detalhes no console.")
+alert("A seguir, veja os detalhes do vinho no console.")
+
+
