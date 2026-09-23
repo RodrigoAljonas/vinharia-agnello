0ad7eabf894883bf52b2d6921ca32ca538bca21b
felipe alves <felipealvescosta4@gmail.com>
2026-09-23 20:52:12 -0300

    Termino do codigo javascript e estruturação do readme

--- scr/js/script.js
+++ scr/js/script.js
@@ -6,4 +6,7 @@ let QtdEstoque = prompt("Digite a quantidadade em estoque: ")
 alert("Cadastro realizado! Veja os detalhes no console.")
 alert("A seguir, veja os detalhes do vinho no console.")
 
-
+console.log (`Nome do vinho:  ${NomeVinho}`)
+console.log (`Tipo do vinho:  ${TipoVinho}`)
+console.log (`Safra:  ${Safra}`)
+console.log (`Quantidade em estoque:  ${QtdEstoque}`)
